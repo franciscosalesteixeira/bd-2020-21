7f5d6694b0947993a654b1a0272341ee7b6bf5cc
franciscosalesteixeira <franciscosalesteixeira88@ua.pt>
2021-06-16 16:53:36 +0100

    backup 1

---
+++ Companhia_Ginasios/Companhia_Ginasios/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Companhia_Ginasios
+{
+    public partial class Form1 : Form
+    {
+
+        private Database db = new Database();
+
+        public Form1()
+        {
+            InitializeComponent();
+            displayGym(db.DbServer, db.DbName, db.UserName, db.UserPass);
+        }
+
+        public void displayGym(string dbServer, string dbName, string userName, string userPass)
+        {
+
+            SqlConnection CN = new SqlConnection("Data Source = " + dbServer + " ;" + "Initial Catalog = " + dbName +
+                                                        "; uid = " + userName + ";" + "password = " + userPass);
+
+            try
+            {
+                CN.Open();
+                if (CN.State == ConnectionState.Open)
+                {
+                    db.Logged = true;
+                    SqlCommand sqlcmd = new SqlCommand("SELECT * FROM GymCompany.Ginasio", CN);
+                    SqlDataReader reader;
+                    reader = sqlcmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Ginasios gym = new Ginasios();
+                        gym.NIF = reader["NIF"].ToString();
+                        gym.Telefone = reader["Telefone"].ToString();
+                        gym.Morada = reader["Morada"].ToString();
+                        gym.Gestor = reader["Gestor"].ToString();
+                        listBox1.Items.Add("NIF: " + gym.NIF + " Phone number: " + gym.Telefone 
+                          + " Address: " + gym.Morada + " Manager: " + gym.Gestor);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                db.Logged = false;
+                db.Error = "Failed to display data due to the following error: \r\n" + ex.Message;
+                MessageBox.Show(db.Error, "An Error Occurred");
+            }
+
+            if (CN.State == ConnectionState.Open)
+                CN.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String table = db.getTableContent(db.DbServer, db.DbName, db.UserName, db.UserPass);
+
+            if (!db.Logged)
+                MessageBox.Show(db.Error, "Operation Failed");
+            else
+                MessageBox.Show(table);
+        }
+    }
+}
---
+++ Companhia_Ginasios/Companhia_Ginasios/Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Companhia_Ginasios
+{
+    public partial class Login : Form
+    {
+
+        private Database db = new Database();
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            db.DbLogin(db.DbServer, db.DbName, db.UserName, db.UserPass);
+            if (db.Logged)
+            {
+                this.Hide();
+                Form1 f1 = new Form1();
+                f1.ShowDialog();
+                this.Close();
+            }
+
+            else
+                MessageBox.Show(db.Error, "Login Failed");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit?", "Confirmation",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
